5cddaadc018a6201903c1e102ec2534d3a287ec0
ju1115 <tmdwn0509@gmail.com>
2026-09-17 16:57:16 +0000

    feat(auth): rotate refresh tokens with explicit failure stages (LOREKEEPER-525)

---
+++ src/main/java/com/loresentry/authentication/config/TokenConfiguration.java
@@ -0,0 +1,12 @@
+package com.loresentry.authentication.config;
+
+import com.loresentry.authentication.application.port.in.RefreshUseCase;
+import com.loresentry.authentication.application.port.out.*;
+import com.loresentry.authentication.application.service.RefreshService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class TokenConfiguration {
+    @Bean RefreshUseCase refreshTokens(JwtTokens jwt, RefreshTokenStore store) { return new RefreshService(jwt, store); }
+}
